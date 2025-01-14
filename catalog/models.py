335cad3cd8088c37e47fcb3ed18a6e3a32e11a7e
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-01-14 18:06:13 +0100

    created model Book

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = [
+        ("HARD", "Hardcover"),
+        ("SOFT", "Softcover"),
+    ]
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=COVER_CHOICES,
+        default="HARD"
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        ordering = ["title", "author"]
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
