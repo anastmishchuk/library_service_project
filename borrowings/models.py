8147b6b8b52ab6b3b02180a028612e83a6e8243b
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-01-21 18:50:06 +0100

    create Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from library_service_project import settings
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.ForeignKey(
+        Book, on_delete=models.CASCADE
+    )
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["-borrow_date"]
