798f765046a4cbd15d92c67a8954e1512a987f26
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-01-21 18:51:19 +0100

    added "borrowings" to installed_apps, indicated AUTH_USER_MODEL; added missed settings for JWT

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "books",
+    "borrowings",
     "users",
 ]
+
+AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -138,4 +141,10 @@ REST_FRAMEWORK = {
         "anon": "30/minute",
         "user": "60/minute"
     }
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
 }
