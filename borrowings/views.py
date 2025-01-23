0db3f36a71e341c1af1a94c4c73ab5ff42f92a7f
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-01-23 14:19:16 +0100

    created BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.permissions import IsAdminOrReadOnly
+from borrowings.models import Borrowing
+from borrowings.serializers import (BorrowingSerializer,
+                                    BorrowingListSerializer,
+                                    BorrowingDetailSerializer)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all().select_related(
+        "book", "user"
+    )
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
