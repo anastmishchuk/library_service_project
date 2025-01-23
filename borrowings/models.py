a608e3aaf7c0be97ccd63ee64e644687299881cd
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-01-23 14:17:13 +0100

    renamed book_id, user_id to book & user

--- borrowings/models.py
+++ borrowings/models.py
@@ -5,13 +5,13 @@ from library_service_project import settings
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
-    book_id = models.ForeignKey(
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
         Book, on_delete=models.CASCADE
     )
-    user_id = models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
 
