361299868f6b3f7bdf6447032c55582d2b072692
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-01-31 17:24:10 +0100

    corrected permission_classes

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,22 +1,18 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 
-from books.permissions import IsAdminOrReadOnly
+
 from borrowings.models import Borrowing
 from borrowings.serializers import (BorrowingSerializer,
-                                    BorrowingListSerializer,
                                     BorrowingDetailSerializer)
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.all().select_related(
-        "book", "user"
-    )
+    queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
-        if self.action == "list":
-            return BorrowingListSerializer
         if self.action == "retrieve":
             return BorrowingDetailSerializer
         return BorrowingSerializer
