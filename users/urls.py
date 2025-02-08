dca7d799ea7c309c807a7f75ff38bad7327090b8
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-02-08 22:22:28 +0100

    corrected users urls

--- users/urls.py
+++ users/urls.py
@@ -8,11 +8,11 @@ from rest_framework_simplejwt.views import (
 from users.views import CreateUserView, ManageUserView
 
 urlpatterns = [
-    path("users/", CreateUserView.as_view(), name="create"),
-    path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("users/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("users/me/", ManageUserView.as_view(), name="manage"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "users"
