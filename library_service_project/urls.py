2031a43b928dc644fa07379a687b393e3e3b585b
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-02-27 17:59:56 +0100

    add urls for SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView & static files

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,4 +32,13 @@ urlpatterns = [
     path("api/borrowings/", include("borrowings.urls", namespace="borrowing")),
     path("api/users/", include("users.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+    path("api/schema/",
+         SpectacularAPIView.as_view(),
+         name="schema"),
+    path("api/doc/swagger/",
+         SpectacularSwaggerView.as_view(url_name="schema"),
+         name="swagger-ui",),
+    path("api/doc/redoc/",
+         SpectacularRedocView.as_view(url_name="schema"),
+         name="redoc"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
